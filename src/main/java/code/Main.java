a4d15c6fcaeb07be40607d8b52c9dc11460aba4b
Yulka <barsetca@rambler.ru>
2022-05-17 12:10:33 +0300

    branch4 new Dogs

--- src/main/java/code/Main.java
+++ src/main/java/code/Main.java
@@ -3,5 +3,8 @@ package code;
 public class Main {
     public static void main(String[] args) {
         Cat cat = new Cat("Tom");
+        Dog dog = new Dog("Bob");
+        Dog dogii = new Dog("Sam");
+
     }
 }
