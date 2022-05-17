5dbde9c895e5a40bdc0574e10fc72093bb742900
Yulka <barsetca@rambler.ru>
2022-05-17 10:48:04 +0300

    new branch cat

--- src/main/java/code/Cat.java
+++ src/main/java/code/Cat.java
@@ -1,4 +1,26 @@
 package code;
 
 public class Cat {
+    private String name;
+    private int age;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
